ee0047f1dbc9533009c1b639f610800ad3153faa
Mustafa <mustafaolmezses@icloud.com>
2024-09-28 15:04:54 +0300

    Initial Commit

---
+++ Whisper/WhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperApp.swift
+//  Whisper
+//
+//  Created by Mustafa Ölmezses on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
